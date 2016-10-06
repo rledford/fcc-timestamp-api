ebb497f1c1c48c2e427137844afb52e5e7240b64
Ryan Ledford <ryan.c.ledford@gmail.com>
2016-10-06 04:12:31 +0000

    working server.js

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require("express");
 var app = express();
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-app.get("/", function(req, res){
-   res.send("hello world"); 
+app.get("/:time", function(req, res){
+    var unix = Date.parse(req.params.time);
+    if(!unix){
+        unix = parseInt(req.params.time);
+        if(!unix){
+            res.send(JSON.stringify({"unix":null, "natural":null}));
+        }
+        else{
+            unix *= 1000;
+        }
+    }
+    var date = new Date(unix);
+    var ret = {};
+    ret.unix = unix/1000;
+    ret.natural = monthNames[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear();
+    res.send(JSON.stringify(ret));
 });
 
 app.listen(8080, function(){
