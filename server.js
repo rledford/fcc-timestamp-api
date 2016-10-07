c78e8dcda32b5e7dfe73433185fb8c564e5fd1ab
Ryan Ledford <ryan.c.ledford@gmail.com>
2016-10-07 03:31:08 +0000

    added simple html landing page

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname+"/index.html"));
+});
 
 app.get("/:time", function(req, res){
     var unix = Date.parse(req.params.time);
